9064e902e6bcf5adac8ba12b911f57ac5a9c079b
Jordan Jones <jordpjones@gmail.com>
2022-03-02 11:09:43 -0800

    use raw url

--- bird2/99-bird2.sh
+++ bird2/99-bird2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 podman exec unifi-os /bin/bash -c "rm /etc/bird/bird.conf"
-podman exec unifi-os /bin/bash -c "curl -fsSLo /tmp/bird2.deb  https://github.com/Kashalls/udmp-utils/blob/master/bird2/bird2_2.0.9-cznic.1_arm64.deb"
+podman exec unifi-os /bin/bash -c "curl -fsSLo /tmp/bird2.deb https://github.com/Kashalls/udmp-utils/raw/master/bird2/bird2_2.0.9-cznic.1_arm64.deb"
 podman exec unifi-os /bin/bash -c "dpkg -i /tmp/bird2.deb"
 podman exec unifi-os /bin/bash -c "curl -fsSLo /etc/bird/bird.conf https://raw.githubusercontent.com/Kashalls/udmp-utils/master/bird2/bird.conf"
 podman exec unifi-os /bin/bash -c "birdc configure"
