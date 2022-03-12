1ef1c1b70913407d84269bd30264e0f19c9e61aa
Jordan Jones <Kashalls@users.noreply.github.com>
2022-03-11 18:31:18 -0800

    Update 99-bird2.sh

--- bird2/99-bird2.sh
+++ bird2/99-bird2.sh
@@ -4,4 +4,5 @@ podman exec unifi-os /bin/bash -c "apt install libssh-gcrypt-4 -y"
 podman exec unifi-os /bin/bash -c "curl -fsSLo /tmp/bird2.deb https://github.com/Kashalls/udmp-utils/raw/master/bird2/bird2_2.0.9-cznic.1_arm64.deb"
 podman exec unifi-os /bin/bash -c "dpkg -i /tmp/bird2.deb"
 podman exec unifi-os /bin/bash -c "curl -fsSLo /etc/bird/bird.conf https://raw.githubusercontent.com/Kashalls/udmp-utils/master/bird2/bird.conf"
-podman exec unifi-os /bin/bash -c "birdc configure"
+podman exec unifi-os /bin/bash -c "systemctl unmask bird && systemctl enable bird && systemctl start bird && birdc configure"
+
